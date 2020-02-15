7e7d1f235523c6ec8110b5fe9530233811de4df4
Nishant Nirmal <nirmalnishant645@gmail.com>
2020-02-15 14:57:52 +0530

    Fibonacci

--- Apple/Problem#451.py
+++ Apple/Problem#451.py
@@ -2,14 +2,9 @@
 Implement the function fib(n), which returns the nth number in the Fibonacci sequence, using only O(1) space
 '''
 def fib(n):
-    a = 0
-    b = 1
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        for i in range(2, n):
-            c = a + b
-            a, b = b, c
-        return b
+    if n <= 1:
+        return n
+    a, b = 0, 1
+    for i in range(2, n):
+        a, b = b, a + b
+    return b
